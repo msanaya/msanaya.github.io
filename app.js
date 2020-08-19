1d0d6bacb12a03d415cc994b37ad7f316bf3de8b
msanaya <50601483+msanaya@users.noreply.github.com>
2020-08-19 19:30:18 -0400

    Update 7.30pm

--- app.js
+++ app.js
@@ -2,60 +2,85 @@ console.log("I'm connected");
 const url = "https://ghibliapi.herokuapp.com/people";
 // console.log(apiList);
 
-
 async function getData() {
   const url = "https://ghibliapi.herokuapp.com/people/"
   try {
+    let name = document.getElementById("name")
+    let gender = document.getElementById("gender")
+    let eyeColor = document.getElementById("eyeColor")
     const response = await axios.get(url)
     console.log(response.data)
     const fileData = response.data
-    fileData.forEach((characterName) => {
-      const character = characterName.name
-      console.log(character)
+    fileData.forEach((character) => {
+      // name.innerText = character.name
+      // console.log(character.name)
+      // console.log(character)
       const dropdown = document.getElementById('select-character')
       // const select = document.querySelector('select')
       const option = document.createElement('option')
       // option.value = `${characterName.name}`
-      option.textContent = `${characterName.name}`
+      option.textContent = `${character.name}`
       dropdown.append(option)
+      console.log(option.textContent)
 
     })
+    //!Create function to remove character result from dom
+    //*Create function to show results in the dom
+
+    showResults(fileData)
+    // console.log(showResults);
     // peopleList(fileData)
+    let opt = sel.options[sel.selectedIndex];
+    console.log(opt)
+
     return fileData
+
+
   } catch (error) {
     // console.log(`Error: ${error}`)
   }
 }
 
-getData();
+getData()
 
-// function peopleList(fileData) {
-//   fileData.forEach((character) => {
-//     console.log(character.name)
-//     const select = document.querySelector('select')
-//     const option = document.createElement('option')
-//     option.value = `${character.name}`
-//     option.textContent = `${character.name}`
-//     select.append(option)
-//   })
-// }
 
-// function peopleList(character) {
-//   console.log(character);
-// }
+function display() {
+  var index = document.getElementById("select-character").selectedIndex;
+  console.log(index)
+}
 
-// apiList.forEach();
+display()
 
-// async function getData() {
-//     try {
-//       const response = await axios.get(url)
-//       console.log(response.data)
-//         .then((character) => {
-//           let fileData = response.data
-//           console.log(fileData)
-//         })
-//       // const fileData = response.data
-//       catch (error) {
-//         console.log(`Error: ${error}`)
 
-//       }
+function showResults(fileData) {
+  // console.log(fileData)
+  characters = fileData
+
+  // `${characterName.name}`
+  let name = document.getElementById("name")
+  let gender = document.getElementById("gender")
+  let eyeColor = document.getElementById("eyeColor")
+  let index = characters.indexOf("Yuki")
+  name.innerText = characters[index].name
+}
+
+const button = document.getElementById("submit").addEventListener("click", () => {
+
+  // console.log(results);
+})
+
+// const submit = document.querySelector('#resultsDiv')
+// // Write eventHandler here
+// submit.addEventListener('submit', (e) => {
+//   e.preventDefault()
+//   const inputValue = document.querySelector('#select-character').value
+//   document.querySelector('#select-character').value = ''
+//   // console.log(inputValue)
+//   fetchData(inputValue)
+// })
+const results = document.getElementById("select-character");
+// console.log(results.options.index)
+// let selectedText = results.options[results.index].text;
+// console.log(selectedText)
+// ? Console log to get specific values mentioning both the index and the key
+// ? console.log(characters[0].gender) 
